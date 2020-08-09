c6b3cd429d10709480758232d5b3346f38c0356b
Sherly1001 <Sherly1001@users.noreply.github.com>
2020-08-09 21:34:09 +0000

    Sher-Noob

--- dwm/config.h
+++ dwm/config.h
@@ -21,7 +21,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "S", "h", "e", "r", "-", "N", "o", "o", "b" };
 
 static const Rule rules[] = {
     /* xprop(1):
